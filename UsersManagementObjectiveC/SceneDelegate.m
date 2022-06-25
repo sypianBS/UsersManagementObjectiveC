d1295607abf245f2d1f4c7a9d4ba0607b1993aa8
sypianBS <sypianbs@gmail.com>
2022-06-25 17:24:43 +0200

    remove storyboard

--- UsersManagementObjectiveC/SceneDelegate.m
+++ UsersManagementObjectiveC/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -18,6 +19,14 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    UIWindowScene * windowScene = (UIWindowScene*) scene;    
+    UIViewController * usersViewController = [[ViewController alloc] init];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window setBackgroundColor:[UIColor redColor]];
+    [self.window setRootViewController: usersViewController];
+    [self.window makeKeyAndVisible];
+    self.window.windowScene = windowScene;
 }
 
 
