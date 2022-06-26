5a0fafefaf83d81724e40678485e230844d82bd9
sypianBS <sypianbs@gmail.com>
2022-06-26 14:50:37 +0200

    add table view programmatically

--- UsersManagementObjectiveC/ViewControllers/Tabs/UsersViewController.h
+++ UsersManagementObjectiveC/ViewControllers/Tabs/UsersViewController.h
@@ -7,8 +7,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface UsersViewController : UIViewController
+@interface UsersViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
 
+
+- (void) setupTableView;
+
+@property NSArray *namesArray;
 
 @end
 
--- UsersManagementObjectiveC/ViewControllers/Tabs/UsersViewController.m
+++ UsersManagementObjectiveC/ViewControllers/Tabs/UsersViewController.m
@@ -17,7 +17,39 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = UIColor.whiteColor;
+    _namesArray = [NSArray arrayWithObjects:@"John",@"Chris",@"Mary",nil];
+    [self setupTableView];
 }
 
+- (void)setupTableView {
+    UITableView *usersTableView = [[UITableView alloc] init];
+    usersTableView.frame = self.view.frame;
+    usersTableView.delegate = self;
+    usersTableView.dataSource = self;
+    [usersTableView registerClass: [UITableViewCell class] forCellReuseIdentifier:@"UserCell"];
+    [self.view addSubview: usersTableView];
+}
+
+//delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _namesArray.count;
+}
+
+//data source
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"UserCell";
+      UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier   forIndexPath:indexPath] ;
+
+      if (cell == nil)
+      {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+      }
+      cell.textLabel.text=[_namesArray objectAtIndex:indexPath.row];
+      return cell;
+}
 
 @end
